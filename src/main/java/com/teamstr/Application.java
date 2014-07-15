92efb7a7bfe79ccb252a15890a9a6e592acce6f1
Mark Ryan <markryan1988@gmail.com>
2014-07-14 20:52:28 -0400

    Messing around w Spring Boot and Thymeleaf

---
+++ src/main/java/com/teamstr/Application.java
@@ -0,0 +1,19 @@
+package com.teamstr;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class Application extends SpringBootServletInitializer{
+
+   
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
--- src/main/java/com/teamstr/controller/Home.java
+++ src/main/java/com/teamstr/controller/Home.java
@@ -1,21 +1,14 @@
 package com.teamstr.controller;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
-@EnableAutoConfiguration
+@Controller
 public class Home {
-
-    @RequestMapping("/")
+	
+	@RequestMapping("/")
     String home() {
-        return "Hello World!";
-    }
-
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(Home.class, args);
+        return "index";
     }
 
 }
